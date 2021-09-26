468a9f8c5e4e993b86f4e5cb472ddf30fa4fe25d
Plamen2212 <86384581+Plamen2212@users.noreply.github.com>
2021-09-26 16:00:36 +0300

    Delete maxeven.js

--- maxeven.js
+++
@@ -1,9 +0,0 @@
-function findMaxEven(numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        const element = numbers[i];
-        if (numbers[i]%2===0 && numbers[i]>element ) {
-            evenNumbers =   element;
-        }
-        
-    }
-}
